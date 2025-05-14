09bd04eb65d4118441176910760a639b4e730892
mompei <ogunborjey@gmail.com>
2025-05-14 14:13:29 +0100

    updated vault state

--- program/src/state/vault_state.rs
+++ program/src/state/vault_state.rs
@@ -18,27 +18,28 @@ impl DataLen for VaultState {
     const LEN: usize = core::mem::size_of::<VaultState>();
 }
 
-impl Initialized for Fundraiser {
+impl Initialized for VaultState {
     fn is_initialized(&self) -> bool {
         self.is_initialized
     }
 }
 
 impl VaultState {
-    pub fn initialize(&mut self, 
-        pub user: Pubkey, 
-        pub expiration: i64,
-        pub amount: u64,
-        pub sol_price_at_initialization: u64,
-        pub vault_bump: u8,
-        pub state_bump: u8,
+    pub fn initialize(
+        &mut self,
+        user: Pubkey,
+        expiration: i64,
+        amount: u64,
+        sol_price_at_initialization: u64,
+        vault_bump: u8,
+        state_bump: u8,
     ) {
-      self.is_initialized = true;
-      self.user = user;
-      self.expiration = expiration;
-      self.amount = amount;
-      self.sol_price_at_initialization = sol_price_at_initialization;
-      self.vault_bump =  vault_bump;
-      self.state_bump = state_bump;
+        self.is_initialized = true;
+        self.user = user;
+        self.expiration = expiration;
+        self.amount = amount;
+        self.sol_price_at_initialization = sol_price_at_initialization;
+        self.vault_bump = vault_bump;
+        self.state_bump = state_bump;
     }
 }
